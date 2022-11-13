824ee0bc8c4f7bf2908c6dfee556de272ab77b0f
m42532461 <m42532461@gmail.com>
2022-11-13 11:41:06 +0800

    Update baseURL

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/";
+const BASE_URL = "https://ecommerceapi-vh86.onrender.com/api/";
 const TOKEN = localStorage.getItem("persist:root")
   ? JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user)
       ?.currentUser?.accessToken
